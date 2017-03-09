5ed289804a728fa0d6c5b9b640e9dbfda8475ece
spskauynnda <1033515355@qq.com>
2017-03-09 17:19:39 +0800

    配置yeoman build初始目录    by: spskauynnda

---
+++ demo1-yo/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name demo1YoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the demo1YoApp
+ */
+angular.module('demo1YoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ demo1-yo/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name demo1YoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the demo1YoApp
+ */
+angular.module('demo1YoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
